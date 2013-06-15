27c1fba30abf92ab14316d4d9ad95d8e7075fbe5
longkai <im.longkai@gmail.com>
2013-06-15 12:19:19 +0800

    修改了版权申明。
    之前没细看，多了一个单词Copyright :-(

--- src/main/java/gxu/software_engineering/shen10/market/entity/Admin.java
+++ src/main/java/gxu/software_engineering/shen10/market/entity/Admin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Copyright 2013 Department of Computer Science and Technology, Guangxi University
+ * Copyright 2013 Department of Computer Science and Technology, Guangxi University
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
